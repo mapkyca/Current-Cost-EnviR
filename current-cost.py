b59aee068c29f63d3a4a410839c7dc7b2a036eed
Ermanno Baschiera <ebaschiera@gmail.com>
2014-12-13 13:33:12 +0100

    fix for reading temperature less than 10, because current cost adds a space before the temp number

--- current-cost.py
+++ current-cost.py
@@ -103,11 +103,11 @@ def main():
 	
 	try:
 		watts_ex = re.compile('<watts>([0-9]+)</watts>')
-		temp_ex = re.compile('<tmpr>([0-9\.]+)</tmpr>')
+		temp_ex = re.compile('<tmpr>([\ ]?[0-9\.]+)</tmpr>') # when temperature is less than 10, currentcost adds a space before the number
 		time_ex = re.compile('<time>([0-9\.\:]+)</time>')
 		
 		watts = str(int(watts_ex.findall(data)[0])) # cast to and from int to strip leading zeros
-		temp = temp_ex.findall(data)[0]
+		temp = temp_ex.findall(data)[0].strip() # remove that extra space
 		time = time_ex.findall(data)[0]
 	except:
 		sys.stderr.write("Could not get details from device")
